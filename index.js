297d2af35da3f6afb5f05f0fd89387062a75c5d4
zhangsa <sandy.zhang@tabcorp.com.au>
2020-07-28 11:46:42 +1000

    done

--- index.js
+++ index.js
@@ -1,5 +1,54 @@
 const projects = require('./data-store');
-let server;
+const server = require('http').createServer();
+
+let projectsList = projects;
+
+server.on('request', (req, res, next) => {
+
+  const { url, method, headers} = req;
+  switch(url) {
+    case '/projects': {
+
+      if(method === 'GET'){
+
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(projectsList));
+
+      }else if (req.method === 'POST') {
+
+        let body = [], project;
+        req.on('error', (err) => {
+          console.error(err);
+        }).on('data', (chunk) => {
+          body.push(chunk);
+        }).on('end', () => {
+          body = Buffer.concat(body).toString();
+          let inValid = body.indexOf('{') === -1;
+         
+          project = inValid ? body:  JSON.parse(body);
+    
+          const isInvalid = inValid ? true : projects.some(p => p.id === project.id);
+          if(isInvalid) {
+            res.writeHead(400, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify({'message':'BAD REQUEST'}));
+
+          }else {
+            projectsList = [...projects, project];
+           res.writeHead(201, {'Content-Type': 'application/json'});
+           res.end(JSON.stringify(projectsList));
+          }
+         
+        });
+      } 
+    }
+      break;
+    
+    default:
+        res.writeHead(404)
+        res.end('no found')
+        break;
+  }
+})
 
 
 
